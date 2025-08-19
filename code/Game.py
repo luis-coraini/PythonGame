7cb152404bb440d621655c3ac7cd6ded0166c3ac
Luis Felipe Coraini Santos <luisfelipe.coraini@gmail.com>
2025-08-19 12:24:17 -0300

    Level 2 implementation

--- code/Game.py
+++ code/Game.py
@@ -16,8 +16,12 @@ class Game:
             menu_return = menu.run()
             
             if menu_return in (MENU_OPTIONS[0], MENU_OPTIONS[1], MENU_OPTIONS[2]):
-                level = Level(self.window, 'Level1', menu_return)
-                level_return = level.run()
+                player_score = [0, 0]
+                level = Level(self.window, 'Level1', menu_return, player_score)
+                level_return = level.run(player_score)
+                if level_return:
+                    level = Level(self.window, 'Level2', menu_return, player_score)
+                    level_return = level.run(player_score)
             elif menu_return == MENU_OPTIONS[4]:
                 pygame.quit()
                 quit()
--- code/Level.py
+++ code/Level.py
@@ -6,27 +6,32 @@ from pygame import Surface, Rect
 from pygame.font import Font
 from code.Entity import Entity
 from code.EntityFactory import EntityFactory
-from code.Const import WIN_HEIGHT, COLOR_WHITE, MENU_OPTIONS, EVENT_ENEMY, SPAWN_TIME, COLOR_GREEN, COLOR_CYAN
+from code.Const import WIN_HEIGHT, COLOR_WHITE, MENU_OPTIONS, EVENT_ENEMY, SPAWN_TIME, COLOR_GREEN, COLOR_CYAN, EVENT_TIMEOUT, TIMEOUT_STEP, TIMEOUT_LEVEL
 from code.EntityMediator import EntityMediator
 from code.Enemy import Enemy
 from code.Player import Player
 
 class Level:
-    def __init__(self, window, name, game_mode):
+    def __init__(self, window: Surface, name: str, game_mode: str, player_score: list[int]):
         self.window = window
         self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
-        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
-        self.entity_list.append(EntityFactory.get_entity('Player1'))
-        self.timeout = 20000 #20 segundos
+        self.entity_list.extend(EntityFactory.get_entity(self.name + 'Bg'))
+        player = (EntityFactory.get_entity('Player1'))
+        player.score = player_score[0]
+        self.entity_list.append(player)
+        self.timeout = TIMEOUT_LEVEL #20 segundos
         
         if game_mode in [MENU_OPTIONS[1], MENU_OPTIONS[2]]:
-            self.entity_list.append(EntityFactory.get_entity('Player2'))
+            player = (EntityFactory.get_entity('Player2'))
+            player.score = player_score[1]
+            self.entity_list.append(player)
         
         pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
+        pygame.time.set_timer(EVENT_TIMEOUT, TIMEOUT_STEP)
     
-    def run(self):
+    def run(self, player_score: list[int]):
         pygame.mixer_music.load(f'./asset/{self.name}.mp3')
         pygame.mixer_music.play(-1)
         clock = pygame.time.Clock()
@@ -51,7 +56,23 @@ class Level:
                 if event.type == EVENT_ENEMY:
                     choice = random.choice(('Enemy1', 'Enemy2'))
                     self.entity_list.append(EntityFactory.get_entity(choice))
-    
+                if event.type == EVENT_TIMEOUT:
+                    self.timeout -= TIMEOUT_STEP
+                    if self.timeout == 0:
+                        for ent in self.entity_list:
+                            if isinstance(ent, Player) and ent.name == 'Player1':
+                                player_score[0] = ent.score
+                            if isinstance(ent, Player) and ent.name == 'Player2':
+                                player_score[1] = ent.score         
+                        return True
+                    
+                found_player = False
+                for ent in self.entity_list:
+                    if isinstance(ent, Player):
+                        found_player = True
+                
+                if not found_player:
+                    return False
             
             self.level_text(14, f'{self.name} - Timeout: {self.timeout / 1000 :.1f}s', COLOR_WHITE, (10,5))
             self.level_text(14, f'fps: {clock.get_fps() :.0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35))  
